5a20b35f1c1314fd91c8fdfa29bc6ccf8a86dd3e
Ilya <gashimovilya2018@gmail.com>
2023-10-07 17:07:31 +0300

    first commit Food-Udemy

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
 import calc from './modules/calc';
-      import cards from './modules/cards';
-      import forms from './modules/forms';
-      import modal from './modules/modal';
-      import slider from './modules/slider';
-      import tabs from './modules/tabs';
-      import timer from './modules/timer';
-      import { openModal } from './modules/modal';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import tabs from './modules/tabs';
+import timer from './modules/timer';
+import { openModal } from './modules/modal';
 window.addEventListener('DOMContentLoaded', () => {
       const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
       calc();
